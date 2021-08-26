3ac85ae026c232c76158dec5961b301e13e93909
William Wassmann <wmwassmann@hotmail.com>
2021-08-26 15:53:55 -0700

    A simple concept for the skeleton of this game.  Looking to use empty positions and draw out the board as input is given.  Will be using an input method to get from point A to point B. All from my python tutorials memory on this one. Will be updating later to use some better coding practices

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,25 @@
 
+tic = 'X'
+tac = 'O'
+pieceOneA = ' '
+pieceOneB = ' '
+pieceOneC = ' '
+pieceTwoA = ' '
+pieceTwoB = ' '
+pieceTwoC = ' '
+pieceThreeA = ' '
+pieceThreeB = ' '
+pieceThreeC = ' '
 
-print('    |     |    ')
-print('---------------')
-print('    |     |    ')
-print('---------------')
-print('    |     |    ')
+def setPiece(play):
+    pieceOneA = play
+    print('  ' + pieceOneA + '  |   ' + pieceOneB + '  |  '  + pieceOneC + '  ')
+    print('------------------')
+    print('  ' + pieceTwoA + '  |   ' + pieceTwoB + '  |  '  + pieceTwoC + '  ')
+    print('------------------')
+    print('  ' + pieceThreeB + '  |   ' + pieceThreeB + '  |  '  + pieceThreeC + '  ')
+
+
+
+
+setPiece(tic)

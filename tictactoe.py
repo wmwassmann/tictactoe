827cb887de3b7ad0c51841e00e5b1a72b0a77b9d
William Wassmann <wmwassmann@hotmail.com>
2021-08-26 16:43:39 -0700

    Working through using a list with empty positions and then filling in those positions dynamically. First attempt at least

--- tictactoe.py
+++ tictactoe.py
@@ -1,25 +1,67 @@
 
 tic = 'X'
 tac = 'O'
-pieceOneA = ' '
-pieceOneB = ' '
-pieceOneC = ' '
-pieceTwoA = ' '
-pieceTwoB = ' '
-pieceTwoC = ' '
-pieceThreeA = ' '
-pieceThreeB = ' '
-pieceThreeC = ' '
 
-def setPiece(play):
-    pieceOneA = play
-    print('  ' + pieceOneA + '  |   ' + pieceOneB + '  |  '  + pieceOneC + '  ')
+
+
+
+
+
+
+board = [' ', ' ', ' ', ' ', ' ', ' ',  ' ', ' ', ' ']
+
+
+def printBoard():
+    print('  ' + str(board[0]) + '  |   ' + str(board[1]) + '  |  '  + str(board[2]) + '  ')
     print('------------------')
-    print('  ' + pieceTwoA + '  |   ' + pieceTwoB + '  |  '  + pieceTwoC + '  ')
+    print('  ' + str(board[3]) + '  |   ' + str(board[4]) + '  |  '  + str(board[5]) + '  ')
     print('------------------')
-    print('  ' + pieceThreeB + '  |   ' + pieceThreeB + '  |  '  + pieceThreeC + '  ')
+    print('  ' + str(board[6]) + '  |   ' + str(board[7]) + '  |  '  + str(board[8]) + '  ')
 
 
 
+def setPiece(play, pos):
+    board.insert(int(pos), str(play))      
+    printBoard()
+# def setPieceTwo(play): 
+#     pieceOneB = play
+#     printBoard(pieceOneB)
 
-setPiece(tic)
+
+setPiece(tic, 2)
+# setPieceTwo(tac)
+
+
+# class Board: 
+#     def _init_(self, pieceOneA, pieceOneB, pieceOneC, pieceTwoA, pieceTwoB, pieceTwoC, pieceThreeA, pieceThreeB, pieceThreeC):
+#         self.pieceOneA = pieceOneA
+#         self.pieceOneB = pieceOneB
+#         self.pieceOneC = pieceOneC
+#         self.pieceTwoA = pieceTwoA
+#         self.pieceTwoB = pieceTwoB
+#         self.pieceTwoC = pieceTwoC
+#         self.pieceThreeA = pieceThreeA
+#         self.pieceThreeB = pieceThreeB
+#         self.pieceThreeC = pieceThreeC
+
+
+
+# def printBoard(pieceOneA, pieceOneB, pieceOneC, pieceTwoA, pieceTwoB, pieceTwoC, pieceThreeA, pieceThreeB, pieceThreeC):
+#     print('  ' + pieceOneA + '  |   ' + pieceOneB + '  |  '  + pieceOneC + '  ')
+#     print('------------------')
+#     print('  ' + pieceTwoA + '  |   ' + pieceTwoB + '  |  '  + pieceTwoC + '  ')
+#     print('------------------')
+#     print('  ' + pieceThreeA + '  |   ' + pieceThreeB + '  |  '  + pieceThreeC + '  ')
+
+
+    # class Board: 
+#     def _init_(self, pieceOneA, pieceOneB, pieceOneC, pieceTwoA, pieceTwoB, pieceTwoC, pieceThreeA, pieceThreeB, pieceThreeC):
+#         self.pieceOneA = pieceOneA
+#         self.pieceOneB = pieceOneB
+#         self.pieceOneC = pieceOneC
+#         self.pieceTwoA = pieceTwoA
+#         self.pieceTwoB = pieceTwoB
+#         self.pieceTwoC = pieceTwoC
+#         self.pieceThreeA = pieceThreeA
+#         self.pieceThreeB = pieceThreeB
+#         self.pieceThreeC = pieceThreeC

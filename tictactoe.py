176af8cec6b645eed2b2e60bd29e6f1979b7898b
William Wassmann <wmwassmann@hotmail.com>
2021-08-26 17:43:27 -0700

    A very simple tic tac toe using python.  It uses a fairly messy input system, but I am not too familiar with how to make this more condensed just yet.  I can imagine there is a way to do this without using an array and possibly a tupple? But I dont know enough about tupples/coordinates to build a simple game without looking on google for some assistance.  Will be updating after doing some research. Will have a separate code file, same repo.

--- tictactoe.py
+++ tictactoe.py
@@ -1,40 +1,48 @@
 
 tic = 'X'
 tac = 'O'
+board = ['-', '-', '-','-', '-', '-','-', '-', '-']
 
 
-board = [' ', ' ', ' ', ' ', ' ', ' ',  ' ', ' ', ' ']
+# selectPositionFour = input('Player O, select an empty position: ')
+
 
 
 def printBoard():
-    print('  ' + str(board[0]) + '  |   ' + str(board[1]) + '  |  '  + str(board[2]) + '  ')
-    print('------------------')
-    print('  ' + str(board[3]) + '  |   ' + str(board[4]) + '  |  '  + str(board[5]) + '  ')
-    print('------------------')
-    print('  ' + str(board[6]) + '  |   ' + str(board[7]) + '  |  '  + str(board[8]) + '  ')
+    print('  0  |   1   |  2  ')
+    print('  ' + str(board[0]) + '  |   ' + str(board[1]) + '   |  '  + str(board[2]) + '  ')
+    print('-------------------')
+    print('  3  |   4   |  5  ')
+    print('  ' + str(board[3]) + '  |   ' + str(board[4]) + '   |  '  + str(board[5]) + '  ')
+    print('-------------------')
+    print('  6  |   7   |  8  ')
+    print('  ' + str(board[6]) + '  |   ' + str(board[7]) + '   |  '  + str(board[8]) + '  ')
 
 
 
 def setPiece(play, pos):
     board[(int(pos))] = str(play)
     printBoard()
+    
 
 
+selectPositionOne = input('Player X, select an empty position: ')
+setPiece(tic, int(selectPositionOne))
+selectPositionTwo = input('Player O, select an empty position: ')
+setPiece(tac, int(selectPositionTwo))
+selectPositionThree = input('Player X, select an empty position: ')
+setPiece(tic, int(selectPositionThree))
+selectPositionFour = input('Player O, select an empty position: ')
+setPiece(tac, int(selectPositionFour))
+selectPositionFive = input('Player X, select an empty position: ')
+setPiece(tic, int(selectPositionFive))
+selectPositionSix = input('Player O, select an empty position: ')
+setPiece(tac, int(selectPositionSix))
+selectPositionSeven = input('Player X, select an empty position: ')
+setPiece(tac, int(selectPositionSeven))
+selectPositionEight = input('Player O, select an empty position: ')
+setPiece(tic, int(selectPositionEight))
+selectPositionNine = input('Player X, select an empty position: ')
+setPiece(tac, int(selectPositionNine))
 
 
-
-def setPieceTwo(play, pos):
-    board[(int(pos))] = str(play)     
-    printBoard()
-
-
-
-
-def setPieceThree(play, pos):
-    board[(int(pos))] = str(play) 
-    printBoard()
-
-
-setPiece(tic, 2)
-setPieceTwo(tac, 1)
-setPieceThree(tic, 0)

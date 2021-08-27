8e94fb4862cfaba5c4a87d49ce6eef1663f540ad
William Wassmann <wmwassmann@hotmail.com>
2021-08-26 18:53:02 -0700

    trying to debut the win conditions. I think it is my syntax thats the problem. I know what needs to happen, just not how to do it.

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,10 @@ tic = 'X'
 tac = 'O'
 board = ['-', '-', '-','-', '-', '-','-', '-', '-']
 
+
 def printBoard():
+    print('                   ')
+    print('                   ')
     print('  0  |   1   |  2  ')
     print('  ' + str(board[0]) + '  |   ' + str(board[1]) + '   |  '  + str(board[2]) + '  ')
     print('-------------------')
@@ -11,11 +14,34 @@ def printBoard():
     print('  ' + str(board[3]) + '  |   ' + str(board[4]) + '   |  '  + str(board[5]) + '  ')
     print('-------------------')
     print('  6  |   7   |  8  ')
-    print('  ' + str(board[6]) + '  |   ' + str(board[7]) + '   |  '  + str(board[8]) + '  ')
+    print('  ' + str(board[6]) + '  |   ' + str(board[7]) + '   |  '  + str(board[8]) + '  ') 
+printBoard()
 
 def setPiece(play, pos):
-    board[(int(pos))] = str(play)
-    printBoard()
+    board[(int(pos))] = str(play)    
+    printBoard()          
+    
+    if ((board[0] and board[1] and board[2]) == 'X' or 
+        (board[0] and board[3] and board[6]) == 'X' or 
+        (board[0] and board[4] and board[8]) == 'X' or
+        (board[1] and board[4] and board[7]) == 'X' or
+        (board[2] and board[5] and board[8]) == 'X' or
+        (board[2] and board[4] and board[6]) == 'X' or
+        (board[3] and board[4] and board[5]) == 'X' or
+        (board[6] and board[7] and board[8]) == 'X'):
+            print('Player X is the Winner!')
+
+        
+            
+    # if ((board[0] and board[1] and board[2] == 'O') or 
+    #     (board[0] and board[3] and board[6] == 'O') or 
+    #     (board[0] and board[4] and board[8] == 'O') or
+    #     (board[1] and board[4] and board[7] == 'O') or
+    #     (board[2] and board[5] and board[8] == 'O') or
+    #     (board[2] and board[4] and board[6] == 'O') or
+    #     (board[3] and board[4] and board[5] == 'O') or
+    #     (board[6] and board[7] and board[8] == 'O')):
+    #         print('Player O is the Winner!')   
 
 selectPositionOne = input('Player X, select an empty position: ')
 setPiece(tic, int(selectPositionOne))
@@ -35,5 +61,10 @@ selectPositionEight = input('Player O, select an empty position: ')
 setPiece(tic, int(selectPositionEight))
 selectPositionNine = input('Player X, select an empty position: ')
 setPiece(tac, int(selectPositionNine))
+
+
+
+
+
 
 

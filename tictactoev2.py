b089b1ce247cdc9cdc3820dcdcae168d1eaa5d41
William Wassmann <wmwassmann@hotmail.com>
2021-08-28 23:10:44 -0700

    building out some more checkpoints. Added in a check if value is a valid numeric input, and also added some dynamic responses to input mistakes. Learning a lot about Python as I go.

--- tictactoev2.py
+++ tictactoev2.py
@@ -1,12 +1,6 @@
 # Global startp variables
 
 
-
-tic = '-x- '
-tac = '-o- '
-
-positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
 newBoard = [
     ['--- ', '--- ', '--- '], 
     ['--- ', '--- ', '--- '],
@@ -45,14 +39,26 @@ def boardCoords(input):
     if column > 2: column = int(column % 3)
     return(row, column)
 
-def checkSpace(coordinates, board):
+def checkSpace(coordinates, board, input):
     row = coordinates[0]
     column = coordinates[1] 
     if board[row][column] == '--- ':
        return False
     else:
+        print("Position " + f'{input + 1} ' + "is already being occupied.")
         print('Please select a different position')
         return True 
+
+def checkValidSpace(input):
+    try:
+        int(input)       
+        return True
+    except:        
+        print(f"'{input}' is not a valid input. Please enter a numeric value of 1-9")  
+        return False
+
+# def winCondition():
+
          
 
 def twoPlayerGame(board):
@@ -62,29 +68,34 @@ def twoPlayerGame(board):
 
     turn = 0
 
-    while board:
+    while turn < 9:
         
         
         if (turn % 2) == 0: 
-           inputO = input('Player O, please select a position: ')     
-           inputO = int(inputO) - 1   
-           coordinates = boardCoords(inputO)
-           if checkSpace(coordinates, newBoard):               
-               continue
-           else:
-               turn = turn + 1
-           setPiece(coordinates, newBoard, turn)
-           
+           inputO = input('Player O, please select a position: ')   
+          
+        #    checkValidSpace(inputO)
+           if checkValidSpace(inputO) == True:
+              inputO = int(inputO) - 1   
+              coordinates = boardCoords(inputO)
+              if checkSpace(coordinates, newBoard, inputO):               
+                  continue
+              else:
+                  turn = turn + 1
+              setPiece(coordinates, newBoard, turn)
+                
           
         else:
            inputX = input('Player X, please select a position: ')
            inputX = int(inputX) - 1
-           coordinates = boardCoords(inputX)
-           if checkSpace(coordinates, newBoard):
-               continue
-           else: 
-               turn = turn + 1
-           setPiece(coordinates, newBoard, turn)
+           checkValidSpace(inputX)
+           if checkValidSpace(inputX):
+               coordinates = boardCoords(inputX)
+               if checkSpace(coordinates, newBoard, inputX):
+                   continue
+               else: 
+                   turn = turn + 1
+               setPiece(coordinates, newBoard, turn)
 
           
 
@@ -105,96 +116,3 @@ def startGame():
 
 startGame()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Takes an automatic play depending on which input function is called. Position is player controlled on input
-# def setPiece(play, pos):
-#     if board[pos] != '-':
-#         print('Please select a different space')
-#         multiplayerGame()
-#         return
-#     else:
-#         board[(int(pos))] = str(play)
-#         printBoard()
-        
-
-#     if ((board[0] == 'X') and (board[1] == 'X') and (board[2] == 'X') or
-#         (board[0] == 'X') and (board[3] == 'X') and (board[6] == 'X') or
-#         (board[0] == 'X') and (board[4] == 'X') and (board[8] == 'X') or
-#         (board[1] == 'X') and (board[4] == 'X') and (board[7] == 'X') or
-#         (board[2] == 'X') and (board[5] == 'X') and (board[8] == 'X') or
-#         (board[2] == 'X') and (board[4] == 'X') and (board[6] == 'X') or
-#         (board[3] == 'X') and (board[4] == 'X') and (board[5] == 'X') or
-#         (board[6] == 'X') and (board[7] == 'X') and (board[8] == 'X')):
-#             print('Player X is the Winner!')
-#             startUp()
-#     if ((board[0] == 'O') and (board[1] == 'O') and (board[2] == 'O') or 
-#         (board[0] == 'O') and (board[3] == 'O') and (board[6] == 'O') or 
-#         (board[0] == 'O') and (board[4] == 'O') and (board[8] == 'O') or
-#         (board[1] == 'O') and (board[4] == 'O') and (board[7] == 'O') or
-#         (board[2] == 'O') and (board[5] == 'O') and (board[8] == 'O') or
-#         (board[2] == 'O') and (board[4] == 'O') and (board[6] == 'O') or
-#         (board[3] == 'O') and (board[4] == 'O') and (board[5] == 'O') or
-#         (board[6] == 'O') and (board[7] == 'O') and (board[8] == 'O')):
-#             print('Player O is the Winner!')
-#             startUp()
-
-
-# # These functions alternate players. It's sloppy, but it gets the job done.  To be reworked as I learn more about Python.
-# def multiplayerGame():  
-#     printBoard()
-#     selectPositionOne = input('Player X, select an empty position: ')    
-#     setPiece(tic, int(selectPositionOne))
-#     selectPositionTwo = input('Player O, select an empty position: ')
-#     setPiece(tac, int(selectPositionTwo))
-#     selectPositionThree = input('Player X, select an empty position: ')
-#     setPiece(tic, int(selectPositionThree))
-#     selectPositionFour = input('Player O, select an empty position: ')
-#     setPiece(tac, int(selectPositionFour))
-#     selectPositionFive = input('Player X, select an empty position: ')
-#     setPiece(tic, int(selectPositionFive))
-#     selectPositionSix = input('Player O, select an empty position: ')
-#     setPiece(tac, int(selectPositionSix))
-#     selectPositionSeven = input('Player X, select an empty position: ')
-#     setPiece(tic, int(selectPositionSeven))
-#     selectPositionEight = input('Player O, select an empty position: ')
-#     setPiece(tac, int(selectPositionEight))
-#     selectPositionNine = input('Player X, select an empty position: ')
-#     setPiece(tic, int(selectPositionNine))
-
-
-# def printBoard():
-#     print('                   ')
-#     print('                   ')
-#     print('  0  |   1   |  2  ')
-#     print('  ' + str(board[0]) + '  |   ' + str(board[1]) + '   |  ' + str(board[2]) + '  ')
-#     print('-------------------')
-#     print('  3  |   4   |  5  ')
-#     print('  ' + str(board[3]) + '  |   ' + str(board[4]) + '   |  ' + str(board[5]) + '  ')
-#     print('-------------------')
-#     print('  6  |   7   |  8  ')
-#     print('  ' + str(board[6]) + '  |   ' + str(board[7]) + '   |  ' + str(board[8]) + '  ')
-
-# # Initialize Board
-
-
-# def startUp():
-
-#     intro = input('Welcome to Tic Tac Toe! How many players: ')
-#     if intro == '2':
-#         printBoard()
-#         multiplayerGame()
-        
-
-# startUp()
